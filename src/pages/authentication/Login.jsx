1de51db5128e30234ce82b7550d97f08d1167acb
im-sanny <raselparvezsanny@gmail.com>
2024-07-22 22:21:10 +0600

    update: updated placeholder

--- src/pages/authentication/Login.jsx
+++ src/pages/authentication/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
                 <input
                   className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
                   type="email"
-                  placeholder="Email"
+                  placeholder="Insert your Email"
                   aria-label="Email"
                 />
               </div>
@@ -36,7 +36,7 @@ const Login = () => {
                 <input
                   className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
                   type="number"
-                  placeholder="Mobile"
+                  placeholder="Insert your mobile number"
                   aria-label="Mobile"
                 />
               </div>
@@ -45,7 +45,7 @@ const Login = () => {
                 <input
                   className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg  dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
                   type="number"
-                  placeholder="Pin"
+                  placeholder="Insert your pin"
                   aria-label="Pin"
                 />
               </div>
